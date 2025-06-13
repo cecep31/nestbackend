55af5bc0468ce3c038854a3072de98f4203d943b
cecep <cecepjanuardi@outlook.com>
2025-06-13 18:09:19 +0700

    fix di

--- src/modules/me/me.module.ts
+++ src/modules/me/me.module.ts
@@ -5,10 +5,12 @@ import { PostsModule } from '../posts/posts.module';
 import { PostsRepository } from '../posts/posts.repository';
 import { TagsService } from '../tags/tags.service';
 import { TagsModule } from '../tags/tags.module';
+import { UsersModule } from '../users/users.module';
+import { UsersService } from '../users/users.service';
 
 @Module({
   controllers: [MeController],
-  providers: [MeService, PostsRepository, TagsService],
-  imports: [PostsModule, TagsModule],
+  providers: [MeService, PostsRepository, TagsService, UsersService],
+  imports: [PostsModule, TagsModule, UsersModule],
 })
 export class MeModule { }
